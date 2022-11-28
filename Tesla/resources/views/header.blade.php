e6eb1208a0573f76f9f6cfd97a6a8fbd158f464f
FlorianBotta <florian.botta@etu.univ-smb.fr>
2022-11-28 10:36:28 +0000

    tout réparer

---
+++ Tesla/resources/views/header.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{asset('css/hamburgeur.css')}}">
+</head>
+<body>
+    
+</body>
+</html>
+
+<header>
+        <div class="left">
+            <a href="/"><img src="{{asset('Models/teslalogo.png')}}" alt="logo_tesla"></a>
+        </div>
+        <div class="right">
+            <img class="see" src="{{asset('Models/Userv2.png')}}">
+            <img class="see" src="{{asset('Models/bagv2.png')}}">
+            <button class="hamburgeur">
+                <div class="bar"></div>
+        </div>
+        </div>
+    </header>
+
+    <nav class="mobile-nav">
+        <a href="/#modelS">Model S</a>
+        <a href="/#model3">Model 3</a>
+        <a href="/#modelX">Model X</a>
+        <a href="/#modelY">Model Y</a>
+        <a href="#shop">Shop</a>
+        <a href="#help">Assistance</a>
+        <span class="ajt-mobile-nav">
+            @if (Route::has('login'))
+                @auth
+                    <a href="{{ url('/profile') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Mon compte</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Se connecter</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">S'inscrire</a>
+                    @endif
+                @endauth
+            @endif
+        </span>
+    </nav>
+
