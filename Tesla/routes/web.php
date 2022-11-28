aa0ccf3850afade5aa35648bf3810074c365a311
FlorianBotta <florian.botta@etu.univ-smb.fr>
2022-11-28 10:08:56 +0000

    authentification

--- Tesla/routes/web.php
+++ Tesla/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,11 +13,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'showMotorisation']);
-//Route::get('/', [HomeController::class, 'showMotorisationModelS']);
-// Route::get('/', [HomeController::class, 'showMotorisationModel3']);
-// Route::get('/', [HomeController::class, 'showMotorisationModelX']);
-// Route::get('/', [HomeController::class, 'showMotorisationModelY']);
-Route::get('/confModelX', [HomeController::class, 'goToConf']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
