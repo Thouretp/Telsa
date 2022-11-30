d28b191d2d57b0f88e4b16c076cee05faff1e809
FlorianBotta <florian.botta@etu.univ-smb.fr>
2022-11-30 09:05:59 +0000

    nothing

--- Tesla/database/factories/UserFactory.php
+++ Tesla/database/factories/UserFactory.php
@@ -19,8 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'surname' => fake()->surname(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'numerotelephone' => fake()->numerotelephone(),
+            'secondprenom' => fake()->secondprenom(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
--- Tesla/resources/views/layouts/navigation.blade.php
+++ Tesla/resources/views/layouts/navigation.blade.php
@@ -1,25 +1,25 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
+        <div class="flex justify-between h-16 ">
+            <div class="flex ">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center ">
                     <a href="/">
                         <img src="{{asset('Models/teslalogoV2.png')}}" alt="logo la tes en folie" class="block h-9 w-auto fill-current text-gray-800">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Tableau de bord') }}
                     </x-nav-link>
                 </div>
-            </div>
+            </div> -->
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden right-0 sm:flex justify-end sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
