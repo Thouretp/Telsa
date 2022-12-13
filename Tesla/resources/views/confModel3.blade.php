33c5493d3f2175c21f77d04dd0a9421a0c9c6bbc
MaxDubb <maxencedubois22@gmail.com>
2022-12-13 08:38:40 +0000

    ajout nouvelle photo visuel

--- Tesla/resources/views/confModel3.blade.php
+++ Tesla/resources/views/confModel3.blade.php
@@ -33,7 +33,7 @@
             <div class="slider" id="modelX">
                 <div class="circle" id="cercle1"></div>
                 <h1 class="title" id="correction_titre">Model 3</h1>
-                <img src="Models/Model 3.jpg"id="MS"/>
+                <img src="{{asset('Models/Models/Model_3/Model3_black._19.png')}}"id="MS"/>
                 <div class="caracteristiques">
                     <div class="infos">
                         <div class="info">
