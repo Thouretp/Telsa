be693009d1be801e224e9b7e49c13fe7d0bc8ea2
FlorianBOTTA <florian.botta@gmail.com>
2023-01-09 09:23:37 +0000

    petite modif

--- Tesla/resources/views/layouts/navigation.blade.php
+++ Tesla/resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
                 </div>
             </div>
 
-             Settings Dropdown -->
+             <!-- Settings Dropdown --> 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown class="align=right" width="48">
                     <x-slot name="trigger">
