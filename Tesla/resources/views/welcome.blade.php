6c3691a1a41c5f4eed555e412c570e9611606aa1
Louis-Jacquin <louis.jacquin73@gmail.com>
2023-01-10 07:39:52 +0000

    Les title sur l'accueil

--- Tesla/resources/views/welcome.blade.php
+++ Tesla/resources/views/welcome.blade.php
@@ -23,10 +23,10 @@
     </div>
 
     <nav class="menu">
-        <p id="lienModelS" class="menuLat"><a href="#modelS">Model S</a></p>
-        <p id="lienModel3" class="menuLat"><a href="#model3">Model 3</a></p>
-        <p id="lienModelX" class="menuLat"><a href="#modelX">Model X</a></p>
-        <p id="lienModelY" class="menuLat"><a href="#modelY">Model Y</a></p>
+        <p id="lienModelS" class="menuLat"><a href="#modelS" title="Model S">Model S</a></p>
+        <p id="lienModel3" class="menuLat"><a href="#model3" title="Model 3">Model 3</a></p>
+        <p id="lienModelX" class="menuLat"><a href="#modelX" title="Model X">Model X</a></p>
+        <p id="lienModelY" class="menuLat"><a href="#modelY" title="Model Y">Model Y</a></p>
     </nav>
 
     <div class="container">
@@ -55,7 +55,7 @@
             <h1 class="title" id="correction_titre">Model S</h1>
             <img src="Models/Model S.jpg" id="MS" />
             <form action="confModelS">
-                <input type="submit" class="visu" id="btn1" value="Configuration personalisée"></input>
+                <input type="submit" class="visu" id="btn1" value="Configuration personalisée" title="Configuration personnalisée"></input>
             </form>
 
             <div class="img">
@@ -91,10 +91,10 @@
             <h1 class="title">Model 3</h1>
             <img src="Models/Model 3.jpg" id="M3">
             <form action="confModel3">
-                <input type="submit" class="visu" id="btn1" value="Configuration personalisée"></input>
+                <input type="submit" class="visu" id="btn1" value="Configuration personalisée" title="Configuration personnalisée"></input>
             </form>
             <form action="essai">
-                <input type="submit" class="test-drive visu" id="btn2" value="Réservez un essai"></input>
+                <input type="submit" class="test-drive visu" id="btn2" value="Réservez un essai" title="Réserver un essai"></input>
             </form>
 
         </div>
@@ -122,7 +122,7 @@
             <div class="circle"></div>
             <h1 class="title">Model X</h1>
             <img src="Models/Model X.jpg" id="MX">
-            <a href="/confModelX"><input type="button" class="visu" id="btn1" value="Configuration personalisée"></input></a>
+            <a href="/confModelX"><input type="button" class="visu" id="btn1" value="Configuration personalisée" title="Configuration personnalisée"></input></a>
         </div>
 
         <div class="slider" id="modelY">
@@ -151,10 +151,10 @@
             <h1 class="title">Model Y</h1>
             <img src="Models/Model Y.jpg" id="MY">
             <form action="confModelY">
-                <input type="submit" class="visu" id="btn1" value="Configuration personalisée" />
+                <input type="submit" class="visu" id="btn1" value="Configuration personalisée" title="Configuration personnalisée"/>
             </form>
             <form action="essai">
-                <input type="submit" class="test-drive visu" id="btn2" value="Réservez un essai"></input>
+                <input type="submit" class="test-drive visu" id="btn2" value="Réservez un essai" title="Réserver un essai"></input>
             </form>
 
             <div class="img">
@@ -169,9 +169,9 @@
     ?>
     <div class="container-cookie">
         <div class="nav-cookie">
-            <p>Consentement</p>
-            <p>Détails</p>
-            <p id="rightSide-cookie">A propos des cookies</p>
+            <p title="Consentement">Consentement</p>
+            <p title="Détails">Détails</p>
+            <p id="rightSide-cookie" title="A propos des cookies">A propos des cookies</p>
         </div>
         <div class="details">
             <p> <strong> Ce site Utilise des cookies. <br></strong>
@@ -192,28 +192,28 @@
                 <p class="turnOverButton"> Marketing</p>
             </div>
             <div class="buttonToggle-cookie">
-                <label class="switch-cookie">
+                <label class="switch-cookie" title="Cookies nécessaires">
                     <input type="checkbox">
                     <span class="slider-cookie round"></span>
                 </label>
-                <label class="switch-cookie">
+                <label class="switch-cookie" title="Cookies de préférences">
                     <input type="checkbox">
                     <span class="slider-cookie round"></span>
                 </label>
-                <label class="switch-cookie">
+                <label class="switch-cookie" title="Cookies de statistiques">
                     <input type="checkbox">
                     <span class="slider-cookie round"></span>
                 </label>
-                <label class="switch-cookie">
+                <label class="switch-cookie" title="Cookies de marketing">
                     <input type="checkbox" checkbox="checked">
                     <span class="slider-cookie round"></span>
                 </label>
             </div>
         </div>
         <div class="submit">
-            <a href="?false-cookie" class="clasic-cookie">Refuser</a>
-            <input class="clasic-cookie" type="button" value="Autoriser la selection" />
-            <a href="?accepte-cookie" class="check-cookie">Tout autoriser</a>
+            <a href="?false-cookie" class="clasic-cookie" title="Refuser les cookies">Refuser</a>
+            <input class="clasic-cookie" type="button" value="Autoriser la selection" title="Autoriser la selection de cookies"/>
+            <a href="?accepte-cookie" class="check-cookie" title="Autoriser tous les cookies">Tout autoriser</a>
     </div>
 <?php
         }
